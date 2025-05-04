49b1c4534a15ca4b89efde379d7fe643f7a47139
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-05-04 13:19:44 +0530

    day 1 basic of data types

---
+++ Hello.java
@@ -0,0 +1,21 @@
+class Hello{;
+public static void main(String a[]){
+    int num1 = 5;
+    int num2 = 10;
+    int result = num1 + num2;
+    // creating the float value
+    float marks = 6.5f;
+    byte by = 125; // Range from -127 to 127
+    short sh = 130;
+    long l = 5854l;
+    double d = 5854;
+    char c = 'k';
+    boolean b = true;
+
+    System.out.println(result);
+
+    // Data Types
+    // 1) Primitive types -> Integers ->Byte,short,init,long, Float->double ,float, character, Boolean
+    // 2) Non Primitive
+}
+}
