33cb3f2198f2e78acdffc9f3804a20e496058773
MANISHKUMAR1999 <panjiyarmanish444@gmail.com>
2025-05-06 19:00:42 +0530

    changes related to basic of java airhtematic operartion

--- Hello.java
+++ Hello.java
@@ -4,15 +4,56 @@ public static void main(String a[]){
     int num2 = 10;
     int result = num1 + num2;
     // creating the float value
-    float marks = 6.5f;
-    byte by = 125; // Range from -127 to 127
-    short sh = 130;
-    long l = 5854l;
-    double d = 5854;
+    // float marks = 6.5f;
+    // byte by = 125; // Range from -127 to 127
+    // short sh = 130;
+    // long l = 5854l;
+    // double d = 5854;
+    // char c = 'k';
+    // boolean b = true;
     char c = 'k';
-    boolean b = true;
+    c++;
 
-    System.out.println(result);
+    // Type Conversion and Casting
+   // byte b  = 127;
+    int d = 257;
+    byte k = (byte) d;
+    System.out.println(k);
+    float f = 5.6f;
+
+    int x = (int) f;
+    // type promotion
+
+    byte e = 123;
+    byte g = 120;
+
+    int result1 = e * g;
+    System.out.println(result1);
+
+    // Arithmetic Operation
+    int y = 100;
+    int z = 900;
+
+    //int result2  = y + z;
+   // int result2  = y - z;
+//    int result2  = y * z;
+//int result2  = y % z;
+        int result2  = y / z;
+
+        // Increment
+        //int result3 = y++; // post incremnt
+        int result3 = ++y; // pre incremnt
+
+    System.out.println(result3);
+
+
+
+
+
+
+
+
+    System.out.println(x);
 
     // Data Types
     // 1) Primitive types -> Integers ->Byte,short,init,long, Float->double ,float, character, Boolean
